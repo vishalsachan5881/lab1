dc8ebe7004dc9294df9c31e8d254ce3709922854
Vishal Sachan <sachanvishal5881@gmail.com>
2022-06-02 11:14:49 -0700

    Initial commit

---
+++ app/src/main/java/com/example/trial1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.trial1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle s) {
+        super.onCreate(s);
+        setContentView(R.layout.activity_main);
+    }
+}
